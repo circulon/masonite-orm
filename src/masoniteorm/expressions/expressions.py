f17a41153cb6bb976a8d4b6659cecb9600816924
Kieren Eaton <499977+circulon@users.noreply.github.com>
2026-06-28 09:34:12 +0800

    fixed case-sensitive direction in OrderByExpression

--- src/masoniteorm/expressions/expressions.py
+++ src/masoniteorm/expressions/expressions.py
@@ -127,13 +127,13 @@ class OrderByExpression:
         self.direction = direction
         self.bindings = bindings
 
-        if raw is False:
-            if self.column.endswith(" desc"):
-                self.column = self.column.split(" desc")[0].strip()
+        if not raw:
+            col_lower = self.column.lower()
+            if col_lower.endswith(" desc"):
+                self.column = self.column[:-5].strip()
                 self.direction = "DESC"
-
-            if self.column.endswith(" asc"):
-                self.column = self.column.split(" asc")[0].strip()
+            elif col_lower.endswith(" asc"):
+                self.column = self.column[:-4].strip()
                 self.direction = "ASC"
 
 
