3b221aae7f3b63da90c0ee8d597febf3b155ea69
Kieren Eaton <499977+circulon@users.noreply.github.com>
2026-08-08 10:21:26 +0800

    added type hints for softDeleteMixin

--- src/masoniteorm/scopes/SoftDeletesMixin.py
+++ src/masoniteorm/scopes/SoftDeletesMixin.py
@@ -1,4 +1,9 @@
+from typing import TYPE_CHECKING
+
 from .SoftDeleteScope import SoftDeleteScope
+
+if TYPE_CHECKING:
+    from ..query import QueryBuilder
 
 
 class SoftDeletesMixin:
@@ -8,6 +13,17 @@ class SoftDeletesMixin:
 
     def boot_SoftDeletesMixin(self, builder):
         builder.set_global_scope(SoftDeleteScope(self.__deleted_at__))
+
+    if TYPE_CHECKING:
+
+        @staticmethod
+        def with_trashed() -> QueryBuilder: ...
+        @staticmethod
+        def only_trashed() -> QueryBuilder: ...
+        @staticmethod
+        def force_delete() -> QueryBuilder: ...
+        @staticmethod
+        def restore() -> QueryBuilder: ...
 
     def get_deleted_at_column(self):
         return self.__deleted_at__
